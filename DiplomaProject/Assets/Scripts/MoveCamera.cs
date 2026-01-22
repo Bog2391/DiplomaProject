6f8fd3e4f4ba7477b214fac09839e1bcb4ed8330
Bohdan Domin <23022005domin.com@gmail.com>
2026-01-22 19:07:33 +0100

    New Player Movement and Camera
    Update Movement and Camera

---
+++ DiplomaProject/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+
+    public Transform cameraPosition;
+    private void Start()
+    {
+        transform.position = cameraPosition.position;
+    }
+    private void Update()
+    {
+        transform.position = cameraPosition.position;
+    }
+}
